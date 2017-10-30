1633f0deab7b43c89cc163e8815763d6820d409d
hananmajali <hananmajali@gmail.com>
2017-10-29 21:25:54 -0400

    our work

---
+++ MyFirstProject/src/com/company/MyClass.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Random;
+
+public class MyClass {
+    int counter =0;
+public void printMsg (String x){
+    System.out.println(x);
+}
+//public void guessMyNumber(int y){
+//
+//    if(y>0 && y<10){
+//        System.out.println("Congratulation!! You win!!");
+//
+//        counter++;
+//    }
+
+
+}
+
+
+}
